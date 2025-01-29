3d134c6a2b6813f44aed6a4a68756ea8c8deff9b
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-01-29 07:21:36 -0500

    Initial commit

--- mt-si.js
+++ mt-si.js
@@ -4,6 +4,7 @@ import { BE } from 'be-enhanced/BE.js';
 import {dispatchEvent as de} from 'trans-render/positractions/dispatchEvent.js';
 /** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
 /** @import {Actions, PAP, AllProps, AP, BAP} from './ts-refs/mt-si/types' */;
+/** @import {AllProps as beParsedAllProps} from './ts-refs/be-parsed/types' */
 
 /**
  * @implements {Actions}
@@ -34,7 +35,17 @@ class MtSi extends BE {
      * @returns 
      */
     async hydrate(self){
-        console.log({self});
+        const {doEval, enhancedElement} = self;
+        if(doEval){
+            throw 'NI';
+        }
+        const {emc} = await import('be-parsed/emc.js');
+        /**
+         * @type {beParsedAllProps}
+         */
+        const beParsed = await /** @type{any} */(enhancedElement).beEnhanced.whenResolved(emc);
+        const {value} = beParsed;
+        console.log({value});
         return /** @type {PAP} */({
             resolved: true,
         });
